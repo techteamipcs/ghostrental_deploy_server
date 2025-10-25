e12c64552352135abdc8a04b4752c2456230b376
TechTeam <techteam.ipcs@gmail.com>
2025-10-25 12:04:24 +0530

    new deploy

--- server/main.server.mjs
+++
@@ -1,3 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-M2LP2SUZ.mjs";import"./chunk-UC5A2PVC.mjs";import"./chunk-4CRVQODM.mjs";import"./chunk-KRV3IYGH.mjs";import"./chunk-DMAEWEGV.mjs";import"./chunk-QTG2NAFV.mjs";import"./chunk-GCFDXCYH.mjs";import"./chunk-N27UOWDD.mjs";export{a as default};
-//# sourceMappingURL=main.server.mjs.map
